fe9d0200adb15036cc78547a1d64889ea79a76f1
omartounsi <tounsiomat@gmail.com>
2025-06-01 00:50:18 +0100

    Hid scrollvar | Updating positioning

--- src/components/Routes/HTML Lessons/mainHTML.tsx
+++ src/components/Routes/HTML Lessons/mainHTML.tsx
@@ -22,37 +22,36 @@ export const MainHTML = () => {
 
   const navigate = useNavigate();
   return (
-    <div className="bg-zinc-950 h-[200vh] w-full grid grid-cols-12 text-neutral-300 pt-14 scrollbar-custom">
+    <div className="grid w-full min-h-screen grid-cols-12 pb-3 bg-zinc-950 text-neutral-300 pt-14">
       <div className="col-span-1 border-r border-dashed border-neutral-800"></div>
-      <div className="col-span-10">
-        <div className="flex flex-col items-center">
-          {/* CARD */}
-          <div className="max-h-170 w-[96%] border border-neutral-800 mt-10 rounded-xl flex flex-col p-10 overflow-y-scroll scrollbar-custom">
-            {/* TITLE */}
-            <h1 className="text-2xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1] text-neutral-50 mb-4">
-              Chapters
-            </h1>
-            {/* CHAPTERS */}
-            <div className="flex flex-col h-full gap-2">
-              {chapters.map((chapter) => (
-                <div
-                  onClick={() => navigate(`/lessons/html/${chapter.id}`)}
-                  key={chapter.id}
-                  className="flex items-center gap-2 px-4 transition-transform duration-300 border cursor-pointer border-neutral-900 min-h-16 rounded-xl hover:scale-105"
-                >
-                  <div className="flex items-center justify-center text-black border border-white rounded-full h-7 w-7">
-                    <IoCheckmark />
-                  </div>
-                  <p className="max-w-2xl font-light text-foreground text-neutral-300">
-                    {" "}
-                    {chapter.title}
-                  </p>
-                  <div className="ml-auto">
-                    <BsThreeDots />
-                  </div>
+      {/* MAIN */}
+      <div className="flex items-center col-span-10">
+        {/* CARD */}
+        <div className="max-h-170 w-[50%] border border-neutral-800 mt-10 rounded-xl flex flex-col p-10 overflow-y-scroll scrollbar-hide">
+          {/* TITLE */}
+          <h1 className="text-2xl font-bold leading-tight tracking-tighter sm:text-3xl md:text-4xl lg:leading-[1.1] text-neutral-50 mb-4">
+            Chapters
+          </h1>
+          {/* CHAPTERS */}
+          <div className="flex flex-col h-full gap-2">
+            {chapters.map((chapter) => (
+              <div
+                onClick={() => navigate(`/lessons/html/${chapter.id}`)}
+                key={chapter.id}
+                className="flex items-center gap-2 px-4 transition-transform duration-300 border cursor-pointer border-neutral-900 min-h-16 rounded-xl hover:scale-105"
+              >
+                <div className="flex items-center justify-center text-black border border-white rounded-full h-7 w-7">
+                  <IoCheckmark />
                 </div>
-              ))}
-            </div>
+                <p className="max-w-2xl font-light text-foreground text-neutral-300">
+                  {" "}
+                  {chapter.title}
+                </p>
+                <div className="ml-auto">
+                  <BsThreeDots />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
