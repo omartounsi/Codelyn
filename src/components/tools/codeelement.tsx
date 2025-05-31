4ef3414407b9fc1f5933bdeb735d534783dd2c42
omartounsi <tounsiomat@gmail.com>
2025-06-01 00:48:42 +0100

    Codeelement can now handle wrapping for the code snippets

--- src/components/tools/codeelement.tsx
+++ src/components/tools/codeelement.tsx
@@ -10,13 +10,13 @@ export const CodeElement = ({
   styling,
 }: CodeElementProps) => {
   return (
-    <div className="flex flex-col justify-center border p-4 border-neutral-800 rounded-lg gap-1">
+    <div className="flex flex-col justify-center gap-1 p-4 border rounded-lg border-neutral-800">
       <code
-        className={`w-full h-auto py-1 px-3 border border-neutral-800 bg-neutral-800/40 ${styling}`}
+        className={`w-full h-auto py-1 px-3 border border-neutral-800 bg-neutral-800/40 whitespace-pre-wrap break-words overflow-x-auto ${styling}`}
       >
         {codeString}
       </code>
-      <p className="max-w-2xl text-xl font-light text-foreground  text-neutral-100 px-3">
+      <p className="max-w-2xl px-3 text-xl font-light text-foreground text-neutral-100">
         {paragraphString}
       </p>
     </div>
